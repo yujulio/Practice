389fe92db5ee661529b082c9a3cddb8ef0a0b80f
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-09-18 14:08:09 -0430

    Adding uniqueness to email field

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
   validates :name, :lastname, :gender, :email, :country, :presence => true
+  validates :email, :uniqueness => true  
   validates_length_of :twitter, :maximum => 15
 end
